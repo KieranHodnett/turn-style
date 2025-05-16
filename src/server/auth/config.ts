c3e80d30d0501432e10e3adbf8d994c79aa8b88e
Kieran Hodnett <krnhodnett@gmail.com>
2025-05-16 02:40:20 -0400

    Now we are fixing the auth config to use a non pooling connection and avoid auth errors

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -3,34 +3,39 @@ import { PrismaAdapter } from "@auth/prisma-adapter"; // Import from @auth/prism
 import { type DefaultSession, type NextAuthConfig } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
 import { db } from "~/server/db";
+import { PrismaClient } from "@prisma/client";
 
 declare module "next-auth" {
-  interface Session extends DefaultSession {
-    user: {
-      id: string;
-    } & DefaultSession["user"];
-  }
+interface Session extends DefaultSession {
+user: {
+id: string;
+ } & DefaultSession["user"];
+ }
 }
 
+const authPrisma = new PrismaClient({
+  datasourceUrl: process.env.POSTGRES_URL_NON_POOLING,
+});
+
 export const authConfig = {
-  providers: [
-    DiscordProvider({
-      clientId: process.env.AUTH_DISCORD_ID!,
-      clientSecret: process.env.AUTH_DISCORD_SECRET!,
-    }),
-  ],
-  adapter: PrismaAdapter(db), // Use the standard adapter
-  callbacks: {
-    session: ({ session, user }) => ({
-      ...session,
-      user: {
-        ...session.user,
-        id: user.id,
-      },
-    }),
-  },
-  pages: {
-    signIn: "/login",
-  },
-  secret: process.env.AUTH_SECRET,
+providers: [
+DiscordProvider({
+clientId: process.env.AUTH_DISCORD_ID!,
+clientSecret: process.env.AUTH_DISCORD_SECRET!,
+ }),
+ ],
+adapter: PrismaAdapter(authPrisma), // Use the standard adapter
+callbacks: {
+session: ({ session, user }) => ({
+...session,
+user: {
+...session.user,
+id: user.id,
+ },
+ }),
+ },
+pages: {
+signIn: "/login",
+ },
+secret: process.env.AUTH_SECRET,
 } satisfies NextAuthConfig;
